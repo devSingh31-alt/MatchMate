1096129733119487fa8d052aad2f724795f8de68
devSingh31-alt <singhdevkumar96@gmail.com>
2026-02-14 01:35:09 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,55 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def login():
+    return render_template("login.html")
+
+
+@app.route("/next", methods=["POST"])
+def next_screen():
+    username = request.form.get("username")
+    return render_template("choice.html", username=username)
+
+
+
+@app.route("/sports")
+def sports():
+    return render_template("sports.html")
+
+@app.route("/watch")
+def watch():
+    sport = request.args.get("sport")
+    return render_template("watch.html", sport=sport)
+
+@app.route("/play")
+def play():
+    sport = request.args.get("sport")
+    return render_template("play.html", sport=sport)
+
+@app.route("/match")
+def match():
+    return render_template("match.html")
+
+@app.route("/success")
+def success():
+    return render_template("success.html")
+
+@app.route("/watch_match")
+def watch_match():
+    return render_template("watch_match.html")
+
+@app.route("/last")
+def last():
+    username = request.args.get("name","Guest")
+    return render_template("last.html", username=username)
+
+@app.route("/profile")
+def profile():
+    username = request.args.get("user", "Guest")
+    return render_template("profile.html", username=username)
+
+if __name__ == "__main__":
+    app.run(debug=True)
