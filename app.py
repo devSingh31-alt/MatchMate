78b3ff90ad9862d2676a8118a50f8c478676551e
devSingh31-alt <singhdevkumar96@gmail.com>
2026-02-14 21:33:14 +0530

    fix port for render

--- app.py
+++ app.py
@@ -53,4 +53,5 @@ def profile():
     return render_template("profile.html", username=username)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
